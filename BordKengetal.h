43a6dd869740bc4079ed2c5f19d7306ff4a2b5d5
Tama McGlinn <t.mcglinn@gmail.com>
2014-12-28 10:19:33 +0100

    Alle 8 octanten zitten erin; unsigned long long van kengetal gemaakt. Bits shiften nog niet correct.~

--- BordKengetal.h
+++ BordKengetal.h
@@ -1,6 +1,6 @@
 
 
-typedef long long eightBite;
+typedef unsigned long long eightBite;
 
 class BordKengetal {
   const eightBite getal;
@@ -22,11 +22,11 @@ public:
         bedacht |= (gaten[x][y] == knikker);
         bedacht <<= 1;
       }
-      bedacht << 8; //opschuiven zodat we ruimte hebben voor de volgende octant
+      bedacht <<= 1;
     }
-    
+    /*
     // octant rechtsboven en -onder
-    for( int y = 2; x <= 4; y += 2 ){ //boven, dan onder
+    for( int y = 2; y <= 4; y += 2 ){ //boven, dan onder
       for( int x = 3; x <= 6; ++x ){
         bedacht |= (gaten[x][3] == knikker);
         bedacht <<= 1;
@@ -35,10 +35,35 @@ public:
         bedacht |= (gaten[x][y] == knikker);
         bedacht <<= 1;
       }
-      bedacht << 8; //opschuiven zodat we ruimte hebben voor de volgende octant
+      bedacht <<= 1;
     }
     
-    
+    // octant onderrechts en -links
+    for( int x = 4; x >= 2; x -= 2 ){ //rechts, dan links
+      for( int y = 3; y <= 6; ++y ){
+        bedacht |= (gaten[3][y] == knikker);
+        bedacht <<= 1;
+      }
+      for( int y = 4; y <= 6; ++y ){
+        bedacht |= (gaten[x][y] == knikker);
+        bedacht <<= 1;
+      }
+      bedacht <<= 1;
+    }
+
+    // octant linksonder en -boven
+    for( int y = 4; y >= 2; y -= 2 ){ //onder, dan boven
+      for( int x = 3; x >= 0; --x ){
+        bedacht |= (gaten[x][3] == knikker);
+        bedacht <<= 1;
+      }
+      for( int x = 2; x >= 0; --x ){
+        bedacht |= (gaten[x][y] == knikker);
+        bedacht <<= 1;
+      }
+      //niet meer opschuiven!
+    }
+*/
     return BordKengetal(bedacht);
   }
 
