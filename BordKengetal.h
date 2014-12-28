5705b6312fa57afbe85fd4b1c8e81b2bde03783d
Tama McGlinn <t.mcglinn@gmail.com>
2014-12-28 09:57:05 +0100

    Octanten in paren; rechter octanten ook gedaan

--- BordKengetal.h
+++ BordKengetal.h
@@ -12,27 +12,31 @@ public:
   static BordKengetal encodeerBord(){
     eightBite bedacht = 0;
     
-    // octant bovenlinks
-    for( int y = 3; y >= 0; --y ){
-      bedacht |= (gaten[3][y] == knikker);
-      bedacht <<= 1;
+    // octant bovenlinks en -rechts
+    for( int x = 2; x <= 4; x += 2 ){ //links, dan rechts
+      for( int y = 3; y >= 0; --y ){
+        bedacht |= (gaten[3][y] == knikker);
+        bedacht <<= 1;
+      }
+      for( int y = 2; y >= 0; --y ){
+        bedacht |= (gaten[x][y] == knikker);
+        bedacht <<= 1;
+      }
+      bedacht << 8; //opschuiven zodat we ruimte hebben voor de volgende octant
     }
-    for( int y = 2; y >= 0; --y ){
-      bedacht |= (gaten[2][y] == knikker);
-      bedacht <<= 1;
+    
+    // octant rechtsboven en -onder
+    for( int y = 2; x <= 4; y += 2 ){ //boven, dan onder
+      for( int x = 3; x <= 6; ++x ){
+        bedacht |= (gaten[x][3] == knikker);
+        bedacht <<= 1;
+      }
+      for( int x = 4; x <= 6; ++x ){
+        bedacht |= (gaten[x][y] == knikker);
+        bedacht <<= 1;
+      }
+      bedacht << 8; //opschuiven zodat we ruimte hebben voor de volgende octant
     }
-    bedacht << 8; //opschuiven zodat we ruimte hebben voor de volgende octant
-
-    // octant bovenrechts
-    for( int y = 3; y >= 0; --y ){
-      bedacht |= (gaten[3][y] == knikker);
-      bedacht <<= 1;
-    }
-    for( int y = 2; y >= 0; --y ){
-      bedacht |= (gaten[4][y] == knikker);
-      bedacht <<= 1;
-    }
-    bedacht << 8;
     
     
     return BordKengetal(bedacht);
